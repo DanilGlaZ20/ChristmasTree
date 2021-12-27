133c2ad39ac5ec2becd5e05978e8135f00a1cbde
Danil <glazdan17@gmail.com>
2021-12-27 15:36:47 +0300

    Not Ready Project

---
+++ ChristmasTree/ChristmasTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChristmasTree
+{
+    public class ChristmasTree<T> : IAbstractBinarySearchTree<T> where T : IComparable
+    {
+    public ChristmasTree() { }
+
+    public ChristmasTree(Node<T> root)
+    {
+        this.Root = root;
+        this.LChild = root.LChild;
+        this.RChild = root.RChild;
+    }
+    public Node<T> Root { get; private set; }
+    public Node<T>LChild { get; private set; }
+    public Node<T>RChild { get; private set; }
+    
+
+    }
+}
